f7a26bbb63a87aa63c61115aba08758143a90291
divrit <divrit97@gmail.com>
2024-01-31 19:51:51 -0700

    push

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -11,11 +11,40 @@ import { ProjectsService } from '../projects.service';
 })
 export class PortfolioComponent implements OnInit{
 
+
+ 
+  filtering : boolean = false;
   isCollapsed: boolean =  true;
   typescript = false;
 
+  angular : boolean = false;
+  python : boolean = false;
+  springboot : boolean = false;
+
+  java : boolean = false;
+  nodejs : boolean = false;
+  html : boolean = false;
+  css : boolean = false;
+  aws : boolean = false;
+
+
 
  projects = {} as project[];
+ resetFilters() {
+  this.typescript = false;
+
+  this.angular = false;
+  this.python = false;
+  this.springboot = false;
+
+  this.java = false;
+  this.nodejs = false;
+  this.html = false;
+  this.css = false;
+  this.aws = false;
+  this.projects = this.projectService.GetProjects();
+  this.filtering = false;
+}
 
  constructor(private projectService : ProjectsService){
   
@@ -23,18 +52,45 @@ export class PortfolioComponent implements OnInit{
 
 
   ngOnInit(): void {
+
     this.projects = this.projectService.GetProjects();
+    
   }
 
   filter(){
     let filterTags: Tag[] = [];
+    this.filtering = false;
     if(this.typescript){
       filterTags.push(Tag.TYPESCRIPT);
+      this.filtering = true;
     }
-
-
+    if(this.angular){
+      filterTags.push(Tag.ANGULAR);
+      this.filtering = true;
+    }
+    if(this.html){
+      filterTags.push(Tag.HTML);
+      this.filtering = true;
+    }
+    if(this.css){
+      filterTags.push(Tag.CSS);
+      this.filtering = true;
+    }
+    if(this.aws){
+      filterTags.push(Tag.AWS);
+      this.filtering = true;
+    }
+    if(this.springboot){
+      filterTags.push(Tag.SPRINGBOOT);
+      this.filtering = true;
+    }
+    if(this.java){
+      filterTags.push(Tag.JAVA);
+      this.filtering = true;
+    }
 
     this.projects =this.projectService.GetProjectsByFilter(filterTags);
   }
+
 
 }
--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-resume',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./resume.component.css']
 })
 export class ResumeComponent {
+
+  constructor(private renderer : Renderer2){
+
+  }
+
+  downloadFile(){
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target','_blank');
+    link.setAttribute('href', '../../assets/Resume.pdf')
+    link.setAttribute('downlaod', 'Resume.pdf')
+    link.click();
+    link.remove();
+  }
 
 }
